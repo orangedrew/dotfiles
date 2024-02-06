2ec7e5492b5616854a89fba85d0bf099c601f65d
ldruwel <laurensdr@outlook.com>
2024-02-06 16:38:03 +0100

    Initial commit

---
+++ dot_config/nvim/lua/plugins/hologram.lua
@@ -0,0 +1,4 @@
+return {
+  "edluffy/hologram.nvim",
+  auto_display = true, -- WIP automatic markdown image display, may be prone to breaking
+}
---
+++ dot_config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,10 @@
+-- lazygit
+return {
+  {
+    "kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+}
